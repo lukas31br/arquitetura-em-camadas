18e6ac718241cd7551451fe22b46d2e1c3aab197
Your Name <your.email@example.com>
2026-06-14 23:13:34 -0300

    feat: DatasetServiceImpl class and methods

--- src/main/java/com/uel/integracaosql/service/Impl/DatasetServiceImpl.java
+++ src/main/java/com/uel/integracaosql/service/Impl/DatasetServiceImpl.java
@@ -1,4 +1,41 @@
 package com.uel.integracaosql.service.Impl;
 
-public class DatasetServiceImpl {
+import com.uel.integracaosql.model.Dataset;
+import com.uel.integracaosql.repository.DatasetsDAO;
+import com.uel.integracaosql.service.DatasetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DatasetServiceImpl implements DatasetService {
+
+    private final DatasetsDAO datasetsDAO;
+
+    @Autowired
+    public DatasetServiceImpl(DatasetsDAO datasetsDAO) {
+        this.datasetsDAO = datasetsDAO;
+    }
+
+    @Override
+    public List<Dataset> findAll() {
+        return datasetsDAO.findAll();
+    }
+
+    @Override
+    public Optional<Dataset> findById(Long id) {
+        return datasetsDAO.findById(id);
+    }
+
+    @Override
+    public Dataset save(Dataset dataset) {
+        return datasetsDAO.save(dataset);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        datasetsDAO.deleteById(id);
+    }
 }
